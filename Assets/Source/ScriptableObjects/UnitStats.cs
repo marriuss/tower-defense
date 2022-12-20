d6df65f4fc374374c943b4e1ad2c43c0f2c7644c
marriuss <53435883+marriuss@users.noreply.github.com>
2022-12-20 18:18:11 +0400

    Add UnitStats SO and Unit component

---
+++ Assets/Source/ScriptableObjects/UnitStats.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="New UnitStats", menuName ="SO/UnitStats", order =51)]
+public class UnitStats : ScriptableObject
+{
+    [SerializeField] private int _health;
+    [SerializeField] private int _damage;
+    [SerializeField] private float _speed;
+    [SerializeField] private float _attackRate;
+    [SerializeField] private float _attackRange;
+
+    public int Health => _health;
+    public int Damage => _damage;
+    public float Speed => _speed;
+    public float AttackRate => _attackRate;
+    public float AttackRange => _attackRange;
+}
