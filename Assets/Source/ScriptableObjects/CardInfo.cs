2a21449cd7a2ff80770d09e919604344ff4845d8
marriuss <53435883+marriuss@users.noreply.github.com>
2022-12-20 18:19:04 +0400

    Add CardInfo SO and Card class

---
+++ Assets/Source/ScriptableObjects/CardInfo.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardInfo : MonoBehaviour
+{
+    [SerializeField] private int _id;
+    [SerializeField] private Unit _unit;
+    [SerializeField] private int _spawnAmount;
+    [SerializeField] private Rarity _rarity;
+    [SerializeField] private int _mana;
+    [SerializeField] private string _name;
+    [SerializeField] private Sprite _icon;
+
+    public int Id => _id;
+    public Unit Unit => _unit;
+    public int SpawnAmount => _spawnAmount;
+    public Rarity Rarity => _rarity;
+    public int Mana => _mana;
+    public string Name => _name;
+    public Sprite Icon => _icon;
+}
+
+public enum Rarity
+{
+    REGULAR,
+    RARE,
+    LEGEND
+}
---
+++ Assets/Source/Scripts/Cards/Card.cs
@@ -0,0 +1,63 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Card
+{
+    private const int MinLevel = 1;
+    private const int MaxLevel = 10;
+    private const int StartExperiencePointsRequired = 2;
+    private const float RecurrentFormulaComponent = 0.15f;
+
+    public event UnityAction ExperienceStatsChanged;
+
+    public int ExperiencePoints { get; private set; }
+    public int ExperiencePointsRequired { get; private set; }
+    public int Level { get; private set; }
+    public CardInfo CardInfo { get; private set; }
+
+    public Card(CardInfo cardInfo)
+    {
+        CardInfo = cardInfo;
+        Level = 1;
+        ExperiencePoints = 0;
+        InvokeExperienceStatsChanges();
+    }
+
+    public bool TryUpLevel()
+    {
+        bool canUpLevel = ExperiencePoints >= ExperiencePointsRequired && Level < MaxLevel;
+
+        if (canUpLevel)
+            UpLevel();
+
+        return canUpLevel;
+    }
+
+    private void UpLevel()
+    {
+        ExperiencePoints -= ExperiencePointsRequired;
+        Level++;
+        UpExperiencePointsRequired();
+    }
+
+    private void UpExperiencePointsRequiredToLevel(int level)
+    {
+        ExperiencePointsRequired = StartExperiencePointsRequired;
+
+        for (int i = MinLevel; i <= level; i++)
+            UpExperiencePointsRequired();
+    }
+
+    private void UpExperiencePointsRequired()
+    {
+        ExperiencePointsRequired = CalculateExperiencePointsRequired();
+    }
+
+    private int CalculateExperiencePointsRequired() => (int)Math.Floor(ExperiencePointsRequired * RecurrentFormulaComponent + StartExperiencePointsRequired);
+
+    private void InvokeExperienceStatsChanges()
+    {
+        ExperienceStatsChanged.Invoke();
+    }
+}
