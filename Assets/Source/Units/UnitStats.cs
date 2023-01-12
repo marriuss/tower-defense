c44e90da898051b233ec7557cba36357cf6e61b9
marriuss <53435883+marriuss@users.noreply.github.com>
2023-01-12 20:25:49 +0400

    Rename AttackRate to AttackDelay

--- Assets/Source/Units/UnitStats.cs
+++ Assets/Source/Units/UnitStats.cs
@@ -8,12 +8,12 @@ public class UnitStats : ScriptableObject
     [SerializeField] private int _health;
     [SerializeField] private int _damage;
     [SerializeField] private float _speed;
-    [SerializeField] private float _attackRate;
+    [SerializeField] private float _attackDelay;
     [SerializeField] private float _attackRange;
 
     public int Health => _health;
     public int Damage => _damage;
     public float Speed => _speed;
-    public float AttackRate => _attackRate;
+    public float AttackDelay => _attackDelay;
     public float AttackRange => _attackRange;
 }
