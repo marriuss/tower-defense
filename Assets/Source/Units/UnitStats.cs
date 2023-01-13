001d03d8d3f89d940f8ea168a9c876fbe41dbd28
marriuss <53435883+marriuss@users.noreply.github.com>
2023-01-13 13:25:24 +0400

    Add new Armor stat

--- Assets/Source/Units/UnitStats.cs
+++ Assets/Source/Units/UnitStats.cs
@@ -5,15 +5,21 @@ using UnityEngine;
 [CreateAssetMenu(fileName ="New UnitStats", menuName ="SO/UnitStats", order =51)]
 public class UnitStats : ScriptableObject
 {
+    private const int MaxArmor = 50;
+
     [SerializeField] private int _health;
+    [SerializeField, Range(0, MaxArmor)] private int _armor;
     [SerializeField] private int _damage;
     [SerializeField] private float _speed;
     [SerializeField] private float _attackDelay;
     [SerializeField] private float _attackRange;
 
     public int Health => _health;
+    public int Armor => _armor;
     public int Damage => _damage;
     public float Speed => _speed;
     public float AttackDelay => _attackDelay;
     public float AttackRange => _attackRange;
+    
+    public int RecalculateDamage(int damage) => Mathf.CeilToInt(1.0f * _armor / 100 * damage);
 }
