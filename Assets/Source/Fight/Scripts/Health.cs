a6cd27d43b37c4b5d3e3e6fecae1363e23da853f
marriuss <53435883+marriuss@users.noreply.github.com>
2023-01-13 12:59:20 +0400

    Implement NegativeArgumentException

--- Assets/Source/Fight/Scripts/Health.cs
+++ Assets/Source/Fight/Scripts/Health.cs
@@ -2,14 +2,32 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Health
 {
+    private int _health;
+
     public int MaxValue { get; private set; }
-    public int Value { get; private set; }
+    public int Value
+    {
+        get
+        {
+            return _health;
+        }
+        set
+        {
+            value = Math.Clamp(value, 0, MaxValue);
+            _health = value;
+        }
+    }
+
+    public event UnityAction ValueChanged;
 
     public Health(int maxHealth)
     {
+        ThrowExceptionIfNegative(maxHealth);
+
         MaxValue = maxHealth;
         Value = maxHealth;
     }
@@ -22,11 +40,34 @@ public class Health
 
     public void IncreaseValue(int value)
     {
-        Value += value;
+        ThrowExceptionIfNegative(value);
+
+        ChangeValue(Value + value);
     }
 
     public void DecreaseValue(int value)
     {
-        Value -= value;
+        ThrowExceptionIfNegative(value);
+
+        ChangeValue(Value - value);
     }
+
+    private void ChangeValue(int newValue)
+    {
+        Value = newValue;
+        ValueChanged?.Invoke();
+    }
+
+    private void ThrowExceptionIfNegative(int value)
+    {
+        if (value < 0)
+            throw new NegativeArgumentException();
+    }
+}
+
+public class NegativeArgumentException : Exception
+{
+    private const string ExceptionMessage = "Argument cannot be negative.";
+
+    public NegativeArgumentException() : base(ExceptionMessage) { }
 }
