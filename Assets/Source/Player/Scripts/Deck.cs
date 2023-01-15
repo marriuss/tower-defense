fced5aca652fff5f43fb288552ba71128b867e82
marriuss <53435883+marriuss@users.noreply.github.com>
2023-01-15 15:56:52 +0400

    Implement method to place cards in deck

--- Assets/Source/Player/Scripts/Deck.cs
+++ Assets/Source/Player/Scripts/Deck.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,5 +14,18 @@ public class Deck
     public Deck()
     {
         _cards = new List<Card>(Capacity);
+
+        for (int i = 0; i < Capacity; i++)
+            _cards.Add(null);
+    }
+
+    public Card PlaceCard(Card card, int index)
+    {
+        if (0 > index || index >= Capacity)
+            throw new ArgumentOutOfRangeException();
+
+        Card replacedCard = _cards[index];
+        _cards[index] = card;
+        return replacedCard;
     }
 }
