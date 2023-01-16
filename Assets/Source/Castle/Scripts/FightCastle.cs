02a1151c71a7561ec71e933cf0f0a27a6163d9de
marriuss <53435883+marriuss@users.noreply.github.com>
2023-01-16 20:09:12 +0400

    Update castle on fight scene

--- Assets/Source/Castle/Scripts/FightCastle.cs
+++ Assets/Source/Castle/Scripts/FightCastle.cs
@@ -4,21 +4,16 @@ using UnityEngine.Events;
 [RequireComponent(typeof(Tower))]
 public class FightCastle : MonoBehaviour
 {
+    [SerializeField] private Tower _mainTower;
     [SerializeField] private TowerSpawner _towerSpawner;
     [SerializeField] private Tower _towerPrefab;
 
     private CastleFightStats _castleStats;
-    private Tower _tower;
-
-    private void Awake()
-    {
-        _tower = GetComponent<Tower>();
-    }
 
     public void ApplyProgress(CastleFightStats castleStats)
     {
         _castleStats = castleStats;
-        _towerSpawner.AddMainTower(_tower);
+        _towerSpawner.AddMainTower(_mainTower);
         int amount = _castleStats.AdditionalTowers;
 
         if (amount > 0)
