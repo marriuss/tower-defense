db5f85c93508bd7d647ca2c3838f5b056997e51c
marriuss <53435883+marriuss@users.noreply.github.com>
2023-01-24 17:08:48 +0400

    Replace controllers with override controllers

--- Assets/Source/Units/Scripts/AnimationPlayer.cs
+++ Assets/Source/Units/Scripts/AnimationPlayer.cs
@@ -6,11 +6,12 @@ using UnityEngine;
 [RequireComponent(typeof(Animator))]
 public class AnimationPlayer : MonoBehaviour
 {
-    private const string IdleAnimation = "Idle";
-    private const string MoveAnimation = "Move";
+    [SerializeField] private UnitControllerParameters _parameters;
+
+    private const string MovingParameter = "Moving";
     private const string AttackTrigger = "Attack";
     private const string TakeHitTrigger = "TakeHit";
-    private const string DeathAnimation = "Die";
+    private const string DieTrigger = "Die";
 
     private Animator _animator;
 
@@ -19,14 +20,28 @@ public class AnimationPlayer : MonoBehaviour
         _animator = GetComponent<Animator>();
     }
 
+    private void Start()
+    {
+        Dictionary<string, float> parameters = _parameters.Parameters;
+        float value;
+
+        foreach (string parameter in parameters.Keys)
+        {
+            value = parameters[parameter];
+
+            if (value > 0)
+                SetFloat(parameter, value);
+        }
+    }
+
     public void PlayIdleAnimation()
     {
-        PlayAnimation(IdleAnimation);
+        SetBool(MovingParameter, false);
     }
 
     public void PlayMoveAnimation()
     {
-        PlayAnimation(MoveAnimation);
+        SetBool(MovingParameter, true);
     }
 
     public void PlayAttackAnimation()
@@ -41,7 +56,7 @@ public class AnimationPlayer : MonoBehaviour
 
     public void PlayDeathAnimation()
     {
-        PlayAnimation(DeathAnimation);
+        PlayAnimation(DieTrigger);
     }
 
     private void PlayAnimation(string animation)
@@ -49,8 +64,37 @@ public class AnimationPlayer : MonoBehaviour
         _animator.Play(animation);
     }
 
+    private void SetFloat(string parameter, float value)
+    {
+        _animator.SetFloat(parameter, value);
+    }
+
+    private void SetBool(string parameter, bool value)
+    {
+        _animator.SetBool(parameter, value);
+    }
+
     private void SetTrigger(string trigger)
     {
         _animator.SetTrigger(trigger);
     }
+}
+
+[Serializable]
+public struct UnitControllerParameters
+{
+    [SerializeField] private float _idleSpeed;
+    [SerializeField] private float _moveSpeed;
+    [SerializeField] private float _attackSpeed;
+    [SerializeField] private float _takeHitSpeed;
+    [SerializeField] private float _dieSpeed;
+
+    public Dictionary<string, float> Parameters => new Dictionary<string, float>()
+    {
+        { "IdleSpeed", _idleSpeed },
+        { "MoveSpeed", _moveSpeed },
+        { "AttackSpeed", _attackSpeed },
+        { "TakeHitSpeed", _takeHitSpeed },
+        { "DieSpeed", _dieSpeed }
+    };
 }
