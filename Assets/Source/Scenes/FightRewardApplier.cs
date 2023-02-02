52cdf0645afd65213b43342ad04671ee124c073e
RudeGalaxy1010 <Karev007@bk.ru>
2023-02-02 20:24:56 +0300

    Apply reward

--- Assets/Source/Scenes/FightRewardApplier.cs
+++ Assets/Source/Scenes/FightRewardApplier.cs
@@ -1,12 +1,29 @@
 using IJunior.TypedScenes;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FightRewardApplier : MonoBehaviour, ISceneLoadHandler<FightReward>
 {
+    [SerializeField] private Player _player;
+
     public void OnSceneLoaded(FightReward argument)
     {
-        throw new System.NotImplementedException();
+        ApplyMoneyReward(argument);
+        ApplyExperienceReward(argument);
+    }
+
+    private void ApplyMoneyReward(FightReward reward)
+    {
+        _player.Balance.AddMoney(reward.Money);
+    }
+
+    private void ApplyExperienceReward(FightReward reward)
+    {
+        List<Card> cards = _player.Deck.Cards;
+
+        for (int i = 0; i < cards.Count; i++)
+        {
+            cards[i].AddExperiencePoints(reward.CardExperiencePoints);
+        }
     }
 }
