8fb3300d174f81b7f12b39d4c3f6d5403687278a
marriuss <53435883+marriuss@users.noreply.github.com>
2023-02-02 19:02:16 +0400

    Add method to add experience points

--- Assets/Source/Cards/CardInfo.cs
+++ Assets/Source/Cards/CardInfo.cs
@@ -5,19 +5,23 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New CardInfo", menuName = "SO/CardInfo", order = 51)]
 public class CardInfo : ScriptableObject
 {
-    [SerializeField] private int _id;
     [SerializeField] private Unit _unit;
     [SerializeField] private Rarity _rarity;
     [SerializeField] private int _mana;
     [SerializeField] private string _name;
     [SerializeField] private Sprite _icon;
 
-    public int Id => _id;
+    public int Id { get; private set; }
     public Unit Unit => _unit;
     public Rarity Rarity => _rarity;
     public int Mana => _mana;
     public string Name => _name;
     public Sprite Icon => _icon;
+
+    public void SetId(int id)
+    {
+        Id = id;
+    }
 }
 
 public enum Rarity
