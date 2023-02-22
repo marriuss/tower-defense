454b3539c28e60e01b1c1c660bce1d4578456f18
marriuss <53435883+marriuss@users.noreply.github.com>
2023-02-22 17:54:32 +0400

    Show fight tutorial if player plays a level the first time

--- Assets/Source/UI/Scripts/Menus/FightTutorial/FightTutorialController.cs
+++ Assets/Source/UI/Scripts/Menus/FightTutorial/FightTutorialController.cs
@@ -6,6 +6,12 @@ public class FightTutorialController : MenuController
 {
     [SerializeField] private FightTutorialView _view;
 
+    private void Start()
+    {
+        if (PlayerProgressStorage.HasSavings == false && FightSceneLoader.SceneLoadingCount == 1)
+            Open();
+    }
+
     public void Open()
     {
         OpenMenu(_view);
